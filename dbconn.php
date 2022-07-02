b907d579dbed2582397f0cb83ec669dbd66bacc4
vanshshah1013 <88569365+vanshshah1013@users.noreply.github.com>
2022-07-02 12:11:18 +0530

    Add files via upload

---
+++ dbconn.php
@@ -0,0 +1,18 @@
+<?php
+
+$server="localhost";
+$username="root";
+$password="";
+$database="hotel_booking";
+
+$conn=mysqli_connect($server,$username,$password,$database);
+
+if(!$conn){
+   die("try again leter");
+}
+?>
+
+ 
+
+
+
---
+++ header.php
@@ -0,0 +1,63 @@
+
+        <div class="sidebar" data-image="../assets/img/sidebar-5.jpg">
+            <!--
+        Tip 1: You can change the color of the sidebar using: data-color="purple | blue | green | orange | red"
+
+        Tip 2: you can also add an image using data-image tag
+    -->
+            <div class="sidebar-wrapper">
+                <div class="logo">
+                    <img src="../images/logo1.png" alt="logo" style="height:100px; width: 200px; padding-left: 1px;">
+                </div>
+                <ul class="nav">
+                    <li>
+                        <a class="nav-link" href="dashboard.php">
+                            <i class="nc-icon nc-chart-pie-35"></i>
+                            <p>Dashboard</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="nav-link" href="./user.php">
+                            <i class="nc-icon nc-circle-09"></i>
+                            <p>User Profile</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="nav-link" href="./users.php">
+                            <i class="nc-icon nc-notes"></i>
+                            <p>Users</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./hotels.php">
+                            <i class="nc-icon nc-paper-2"></i>
+                            <p>List of Hotels</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="nav-link" href="./admins.php">
+                            <i class="nc-icon nc-atom"></i>
+                            <p>List of admins</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="nav-link" href="./approvals.php">
+                            <i class="nc-icon nc-pin-3"></i>
+                            <p>pending approvals</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="nav-link" href="./inquiry.php">
+                            <i class="nc-icon nc-bell-55"></i>
+                            <p>Inquiry List</p>
+                        </a>
+                    </li>
+                    <li class="nav-item ">
+                        <a class="nav-link" href="upgrade.php">
+                            <i class="nc-icon nc-alien-33"></i>
+                            <p>Add New Admin</p>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
